374ad05371c08bdcd939e964c660f331b1cc9ce9
DevonTheFloor <devonthefloor@free.fr>
2022-06-27 05:32:46 +0200

    Update process env

--- router-proxy.js
+++ router-proxy.js
@@ -4,7 +4,7 @@ const helmet = require('helmet');
 const httpProxy = require('http-proxy')
 const proxy = httpProxy.createProxyServer()
 //const mongoose = require('mongoose');
-const nMailer = require('nodemailer');
+//const nMailer = require('nodemailer');
 const vhost = require('vhost');
 
 /*mongoose.connect(process.env.ROUTER_BD,
@@ -36,7 +36,7 @@ routeur.use('/',(req, res, next)=>{
     }*/
   });
 })
-routeur.use(vhost(SECOND_SITE, function handle (req, res, next) {
+routeur.use(vhost('SECOND_SITE', function handle (req, res, next) {
   // for match of "foo.bar.example.com:8080" against "*.*.example.com":
   console.dir(req.vhost.host) // => 'foo.bar.example.com:8080'
   console.dir(req.vhost.hostname) // => 'foo.bar.example.com'
